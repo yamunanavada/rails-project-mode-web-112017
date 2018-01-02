c131670d8d5532434ddc817cc88bda9cda46daf9
yamunanavada <yamunanavada@gmail.com>
2018-01-02 15:16:37 -0500

    added authorization and views

---
+++ snowball_fight/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user == @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:error] = "Username and password do not match"
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
+  end
+
+
+end
--- snowball_fight/app/models/user.rb
+++ snowball_fight/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :teams
   has_many :leagues, through: :teams
   has_secure_password
+  validates :username, presence: true, uniqueness: true
   
 
 end
