9ff188417e13daec15fc7ebe7b06d7d64e4af6dc
yamunanavada <yamunanavada@gmail.com>
2018-01-03 09:35:16 -0500

    made more edits to games

--- snowball_fight/app/models/user.rb
+++ snowball_fight/app/models/user.rb
@@ -3,6 +3,15 @@ class User < ApplicationRecord
   has_many :leagues, through: :teams
   has_secure_password
   validates :username, presence: true, uniqueness: true
-  
+
+
+  #What's in this user's war_chest
+  # def buy_players
+  # end
+
+  #def win_game
+  #end
+
+
 
 end
