4a526f35803631a9e40e3412becfff85b51f4a06
yamunanavada <yamunanavada@gmail.com>
2018-01-05 12:28:40 -0500

    edits to boot

--- app/controllers/team_players_controller.rb
+++ app/controllers/team_players_controller.rb
@@ -1,18 +1,23 @@
 class TeamPlayersController < ApplicationController
 
 def create
-  @team_player = TeamPlayer.new(team_player_params)
-  @player = Player.find(team_player_params[:player_id])
-  @team = Team.find(team_player_params[:team_id])
-  @team.buy_player(@player.price)
-
-  if @team_player.valid? && @team.valid?
-    @team_player.save
-    @team.save
-    redirect_to team_path(@team)
+  if team_player_params[:team_id] == ""
+    flash[:error] = "You did not select a team that is available."
+    redirect_to players_path
   else
-    flash[:error] = @team_player.errors.full_messages + @team.errors.full_messages
-    redirect_to player_path(Player.find(team_player_params[:player_id]))
+    @team_player = TeamPlayer.new(team_player_params)
+    @player = Player.find(team_player_params[:player_id])
+    @team = Team.find(team_player_params[:team_id])
+    @team.buy_player(@player.price)
+
+    if @team_player.valid? && @team.valid?
+      @team_player.save
+      @team.save
+      redirect_to team_path(@team)
+    else
+      flash[:error] = @team_player.errors.full_messages + @team.errors.full_messages
+      redirect_to player_path(Player.find(team_player_params[:player_id]))
+    end
   end
 end
 
